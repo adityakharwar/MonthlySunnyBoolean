784d4808dedd0d093683e8e50564cf21a6b53f65
Aditya Kharwar <adityakharwar28@gmail.com>
2020-08-27 13:23:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function bird(){
+document.getElementById("bird").showModal();
+}
+
+function game(){
+document.getElementById("game").showModal();
+}
+
+function flower(){
+document.getElementById("flower").showModal();
+}
+
+function animal(){
+document.getElementById("animal").showModal();
+}
